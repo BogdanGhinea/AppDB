bd543d8082fff3656e3199d4874be5ee8c0eeaaa
BogdanGhinea <bogdanghinea84@gmail.com>
2022-08-15 09:29:56 +0200

    DatenBank  Formular and Views added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,5 +19,24 @@ namespace AppDatenB.Controllers
         {            
             return View(ctx.Articles.ToList());
         }
+        [HttpGet]
+        public IActionResult NewArticle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewArticle(Article article)
+        {
+            article.Created = DateTime.Now;
+            int maxId = 0;
+            if (ctx.Articles.Any())
+            {
+                maxId = ctx.Articles.Max(a => a.Id);
+            }
+            article.Id = maxId + 1;
+            ctx.Add(article);
+            ctx.SaveChanges();
+        }
+
     }
 }
